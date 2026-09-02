fc708effab6eecaba11d765953b842aa838b0776
newdevnirmal <nirmalkuttappi2003@gmail.com>
2026-09-02 21:57:36 +0530

    basic player movement mouse, movement , lighting

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -5,18 +5,37 @@ const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
 
 
+
+
+var sensivity = 0.003
+@onready var camera = $Camera3d
+
+func _ready():
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+
+
+func _unhandled_input(event):
+	if event is InputEventMouseMotion:
+		rotate_y(-event.relative.x * sensivity)
+		camera.rotate_x(-event.relative.y * sensivity)
+		camera.rotation.x = clamp(camera.rotation.x,deg_to_rad(-60), deg_to_rad(70))
+
+func _process(delta):
+	if Input.is_action_just_pressed("escape"):
+		get_tree().quit()
+
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	var input_dir := Input.get_vector("left", "right", "up", "down")
 	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
 		velocity.x = direction.x * SPEED
